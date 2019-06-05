5855b6be3b9fbf450f225e553652024e057bbea2
Jen Weber <j@jenweber.me>
2019-06-05 19:47:29 -0400

    assert deprecation warning is shown #31

--- tests/integration/components/deprecations-test.js
+++ tests/integration/components/deprecations-test.js
@@ -1,4 +1,5 @@
 import { module, test } from 'qunit';
+// import helper from 'ember-qunit-assert-helpers';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
@@ -19,18 +20,6 @@ module('Integration | Component | deprecations', function(hooks) {
       </div>
       <div class="source">{{to-elsewhere named="my-target" send=(hash myComponent=(component "x-foo") someText="some text")}}</div>
       `);
-      assert.ok(false); // todo look up the deprecation
-  });
-
-  test('shows a deprecation if a hash includes a component string', async function(assert) {
-    await render(hbs`
-      <div class="my-target">
-        {{#from-elsewhere name="my-target" as |content|}}
-          {{component content.myComponent}}
-        {{/from-elsewhere}}
-      </div>
-      <div class="source">{{to-elsewhere named="my-target" send=(hash myComponent="x-foo" someText="some text")}}</div>
-      `);
-    assert.ok(false); // todo look up the deprecation
+    assert.expectDeprecation(/Sending a component inside a hash/);
   });
 });
