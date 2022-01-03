8d3bb92c326194d76a184bc69d718091bfbd164b
Edward Faulkner <edward@eaf4.com>
2022-01-02 22:44:41 -0500

    port dummy app to glimmer components

--- tests/dummy/app/components/modal-target.js
+++ tests/dummy/app/components/modal-target.js
@@ -1,15 +1,19 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
-export default Component.extend({
-  modalAnimation
-});
+export default class extends Component {
+  modalAnimation = modalAnimation;
+}
 
 function modalAnimation() {
-  return this.lookup('explode').call(this, {
-    pick: '.modal-background',
-    use: ['fade', { maxOpacity: 0.5 }]
-  },{
-    pick: '.modal-dialog',
-    use: 'to-left'
-  });
+  return this.lookup('explode').call(
+    this,
+    {
+      pick: '.modal-background',
+      use: ['fade', { maxOpacity: 0.5 }],
+    },
+    {
+      pick: '.modal-dialog',
+      use: 'to-left',
+    }
+  );
 }
