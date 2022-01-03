23d2cd37a81efa5fec7e6fe7237de5ed7be94c8a
Edward Faulkner <edward@eaf4.com>
2022-01-02 23:43:11 -0500

    updates to snippet handling and some docs

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
     snippetSearchPaths: ['tests/dummy'],
+    'ember-prism': {
+      components: ['markup'],
+    },
   });
 
   /*
