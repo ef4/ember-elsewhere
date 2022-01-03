037dab9106c90ac90750516ad9d44e693dac0f08
Edward Faulkner <edward@eaf4.com>
2022-01-02 22:09:06 -0500

    port to glimmer component

--- addon/components/from-elsewhere.js
+++ addon/components/from-elsewhere.js
@@ -1,46 +1,43 @@
 import { getOwner } from '@ember/application';
 import { schedule } from '@ember/runloop';
 import { Promise } from 'rsvp';
-import { inject as service } from '@ember/service';
-import Component from '@ember/component';
+import { inject } from '@ember/service';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
-export default Component.extend({
-  service: service('ember-elsewhere'),
-  tagName: '',
+export default class extends Component {
+  @inject('ember-elsewhere') service;
 
-  didReceiveAttrs() {
-    if (!this.name) {
-      this.set('name', 'default');
+  get name() {
+    if (this.args.named) {
+      throw new Error(
+        `<FromElsewhere> takes a "@name=" parameter, not "@named="`
+      );
     }
-    if (this.named) {
-      throw new Error(`from-elsewhere takes a "name" parameter, not "named"`);
-    }
-  },
+    return this.args.name ?? 'default';
+  }
 
   // We don't yield any content on the very first render pass, because
   // we want to give any concurrent {{to-elsewhere}} components a chance
   // to declare their intentions first. This allows the components
   // inside us to see a meaningful initial value on their initial
   // render.
-  initialized: false,
+  @tracked
+  initialized = false;
 
-  // we use init here instead of didInsertElement because we want to
-  // take action even in fastboot.
-  init() {
-    this._super();
+  constructor(...args) {
+    super(...args);
 
-    let promise = new Promise(resolve => {
+    let promise = new Promise((resolve) => {
       schedule('afterRender', () => {
-        if (!this.isDestroyed) {
-          this.set('initialized', true);
-        }
+        this.initialized = true;
         resolve();
       });
     });
 
     let fastboot = getOwner(this).lookup('service:fastboot');
-    if (fastboot && fastboot.get("isFastBoot")) {
+    if (fastboot?.isFastBoot) {
       fastboot.deferRendering(promise);
     }
   }
-});
+}
--- addon/components/multiple-from-elsewhere.js
+++ addon/components/multiple-from-elsewhere.js
@@ -1,3 +1,2 @@
 import FromElsewhereComponent from './from-elsewhere';
-
-export default FromElsewhereComponent.extend({});
+export default class extends FromElsewhereComponent {}
