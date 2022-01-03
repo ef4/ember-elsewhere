7c7abd29e7c659f1ea99a516fb051cf7064905cb
Edward Faulkner <edward@eaf4.com>
2022-01-03 02:46:57 -0500

    fixing classic scenario and removing default-with-jquery scenario

---
+++ tests/dummy/app/components/first-sidebar.js
@@ -0,0 +1,3 @@
+// allows us to test with the template-only-glimmer-components flag disabled
+import templateOnlyComponent from '@ember/component/template-only';
+export default templateOnlyComponent();
